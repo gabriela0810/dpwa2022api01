4116d16fd6890c926a55109deaaaa4cec3ead8f9
Jenniffer Roque <jennifferg1008@gmail.com>
2022-02-26 03:04:31 -0600

    Controlador Marcas adicionado y cambios en el controlador

---
+++ EquiposWebAPI/Controllers/marcasController.cs
@@ -0,0 +1,18 @@
+using EquiposWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EquiposWebAPI.Controllers
+{
+    public class marcasController : ControllerBase
+    {
+        
+
+        //configurar mi variable de conexion al contexto db
+        private readonly prestamosContext _contexto;
+
+
+    }
+}

02770bcea20107591682181adb3820ed3878564e
Jenniffer Roque <jennifferg1008@gmail.com>
2022-02-26 01:32:13 -0600

    PRIMERA CARGA

---
+++ EquiposWebAPI/Controllers/EquiposController.cs
@@ -0,0 +1,38 @@
+﻿using EquiposWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EquiposWebAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class EquiposController : ControllerBase
+    {
+        
+
+        //configurar mi variable de conexion al contexto db
+        private readonly prestamosContext _contexto;
+
+        public EquiposController(prestamosContext miContexto)
+        {
+            _contexto = miContexto;
+        }
+
+        [HttpGet]
+        [Route("api/equipos")]
+        
+        public IActionResult Get()
+        {
+            IEnumerable<equipos> equiposList = (from e in _contexto.equipos
+                                                select e);
+
+            if (equiposList.Count() > 0)
+            {
+                return Ok(equiposList);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ EquiposWebAPI/prestamosContext.cs
@@ -0,0 +1,17 @@
+﻿using EquiposWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EquiposWebAPI
+{
+    public class prestamosContext : DbContext
+    {
+        public prestamosContext(DbContextOptions<prestamosContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<equipos> equipos { get; set; }
+
+    }
+}
